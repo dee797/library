b4032b272c52ba27607a00a6f0e914bf99849d4d
dee <178827694+dee797@users.noreply.github.com>
2024-09-24 17:07:39 -0600

    Logic for adding a new book based on user's inputs

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-"use strict";
-
 const addBookBtn = document.querySelector("#addBookBtn");
+const cancelBtn = document.querySelector("#cancel");
 const dialog = document.querySelector("#addBook");
 const form = document.querySelector("form");
+const myLibrary = [];
 let data;
+
 
 
 addBookBtn.addEventListener("click", () => {
@@ -11,24 +12,30 @@ addBookBtn.addEventListener("click", () => {
 });
 
 form.addEventListener("submit", (event) => {
-    data = new FormData(form);
-    event.preventDefault();
+    const tempData = new FormData(form);    //not iterable or an array
+    data = Object.fromEntries(tempData);    //returns dictionary
+    event.preventDefault();     //prevents submission of the form (stops the event)
     dialog.close();
+    addBookToLibrary();
 });
 
-const myLibrary = [];
+cancelBtn.addEventListener("click", () => {
+    dialog.close();
+    form.reset();   //manually reset form
+})
 
 
 
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, bookNum) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.bookNum = bookNum
 
     this.info = function() {
-        if (this.read === true) {
+        if (this.read === "true") {
             return `${this.title} by ${this.author}, ${this.pages} pages,
             already read`;
         } else {
@@ -36,5 +43,9 @@ function Book(title, author, pages, read) {
             not yet read`;
         }
     }
+}
+
+function addBookToLibrary() {
+    myLibrary.push(new Book(data.title, data.author, data.pages, data.read, myLibrary.length));
 }
 
